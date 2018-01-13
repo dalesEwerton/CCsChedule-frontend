507f34da51225e50d05a059ebdfb84baeef041b9
dalesEwerton <dales.fragoso@ccc.ufcg.edu.br>
2018-01-13 04:42:58 -0200

    Components for Login, Create Account and Schedule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { CreateAccountComponent } from './components/create-account/create-account.component';
+import { ScheduleComponent } from './components/schedule/schedule.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CreateAccountComponent,
+    ScheduleComponent
   ],
   imports: [
     BrowserModule
